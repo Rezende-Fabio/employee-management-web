dc57684d4a93265add0fd5c09d08a0571847b1cd
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-28 00:31:16 -0300

    FAET: added page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: "", component: IndexComponent },
-    { path: "index", component: IndexComponent }
+    { path: "index", component: IndexComponent },
+    { path: "home", component: HomeComponent }
 ];
