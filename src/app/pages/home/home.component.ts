60b7b9375cecba953d028f570d37c894d24f0b75
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-28 01:58:29 -0300

    FEAT: adding the sidebar component and styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [SidebarComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
