978590d322e9994a12e9232124091570ee0cd72d
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-30 12:29:27 -0300

    FEAT: created the structure and styling of the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,18 +3,23 @@ import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../models/employee/employee';
+import { ButtonComponent } from "../../components/button/button.component";
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, RouterModule],
+  imports: [SidebarComponent, RouterModule, ButtonComponent, MatFormFieldModule, MatInputModule, MatTableModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
   employees: Employee[] = [];
-  
-  constructor(private apiService: EmployeeService) {}
+  dataSource: any;
+
+  constructor(private apiService: EmployeeService) { }
 
   ngOnInit() {
     this.apiService.getAllEmployees('v1/employees').subscribe({
@@ -26,5 +31,10 @@ export class HomeComponent implements OnInit {
         console.error('Erro ao buscar funcionários:', error);
       },
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 } 
