7902a8542a13107d44704506733fed178bf56ec0
Rezende-Fabio <rezendefabio337@gmail.com>
2024-11-30 13:45:33 -0300

    FEAT: added dataTable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,31 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
 import { Employee } from '../../models/employee/employee';
 import { ButtonComponent } from "../../components/button/button.component";
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent, RouterModule, ButtonComponent, MatFormFieldModule, MatInputModule, MatTableModule],
+  imports: [SidebarComponent, RouterModule, ButtonComponent, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  employees: Employee[] = [];
+  displayedColumns: string[] = ['name', 'department', 'shift', 'action'];
   dataSource: any;
 
-  constructor(private apiService: EmployeeService) { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ListEmployees: Employee[] = [];
+
+  constructor(private apiService: EmployeeService) {
+    this.dataSource = new MatTableDataSource<Employee>(this.ListEmployees);
+  }
 
   ngOnInit() {
+    this.getAllEmployees();
+  }
+
+  ngAfterViewInit() {
+    this.createPagination();
+  }
+
+  createPagination() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  getAllEmployees() {
     this.apiService.getAllEmployees('v1/employees').subscribe({
-      next: (response) => {
-        this.employees = response;
-        console.log('Employees:', this.employees);
+      next: (response: Employee[]) => {
+        this.ListEmployees = response;
+
+        this.dataSource = new MatTableDataSource<Employee>(this.ListEmployees);
+        this.createPagination();
       },
       error: (error) => {
         console.error('Erro ao buscar funcionários:', error);
@@ -36,5 +61,9 @@ export class HomeComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 } 
