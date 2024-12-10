4f0d51ce2acdeefe06f34c9ae9fbfc48c055cb98
Rezende-Fabio <rezendefabio337@gmail.com>
2024-12-09 21:04:25 -0300

    FEAT: create page of create-update-employee

---
+++ src/app/pages/create-update-employee/create-update-employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-employee',
+  imports: [],
+  templateUrl: './create-update-employee.component.html',
+  styleUrl: './create-update-employee.component.css'
+})
+export class CreateUpdateEmployeeComponent {
+
+}
